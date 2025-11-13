c3417a5e11baf2bdc59e4585d354b946a88fd7f5
Rupankar Chakroborty <rupankarchakroborty@Rupankars-MacBook-Air.local>
2025-11-13 13:30:58 +0530

    Initial Commit

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -1,3 +1,6 @@
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src')))
+
 from incident_iq.api.controllers.user_controller import handle_user_request
 from incident_iq.core.config import show_config
 
